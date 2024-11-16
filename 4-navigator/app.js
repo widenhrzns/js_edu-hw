1510c367b329a8f08f1b2e33f786a43a4f7a5da0
widenhrzns <widenhrzns@gmail.com>
2024-11-16 22:19:40 +0300

    5-languages

--- 4-navigator/app.js
+++
@@ -1,4 +0,0 @@
-let addressLat, addressLong, positionLat, positionLong;
-const distance =
-  ((positionLat - addressLat) ** 2 + (addressLong - positionLong) ** 2) ** 0.5;
-console.log(`Расстояние: ${distance}`);
---
+++ 5-languages/app.js
@@ -0,0 +1,13 @@
+const userLanguage = prompt();
+
+switch (userLanguage) {
+  case "en":
+    console.log("Good afternoon!");
+    break;
+  case "ru":
+    console.log("Добрый день!");
+    break;
+  case "de":
+    console.log("Guten tag!");
+    break;
+}
