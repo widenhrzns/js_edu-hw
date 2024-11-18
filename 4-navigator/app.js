47a1e283571fae62dd448acf17a4bda59e9d918b
widenhrzns <widenhrzns@gmail.com>
2024-11-18 12:33:13 +0300

    Исправления 01

--- 4-navigator/app.js
+++ 4-navigator/app.js
@@ -1,4 +1,9 @@
-let addressLat, addressLong, positionLat, positionLong;
+const addressLat = 0,
+  addressLong = 0,
+  positionLat = 1,
+  positionLong = 1;
+
 const distance =
   ((positionLat - addressLat) ** 2 + (addressLong - positionLong) ** 2) ** 0.5;
+
 console.log(`Расстояние: ${distance}`);
