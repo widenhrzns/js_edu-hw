7ecceba1471b96d53cb81043fbb4b53896ec4b91
widenhrzns <widenhrzns@gmail.com>
2024-11-27 23:16:41 +0300

    some exp

--- 11-date-array/app.js
+++ 11-date-array/app.js
@@ -1,42 +1,53 @@
-const dates = [
-  "10-02-2022",
-  "тест",
-  "11/12/2023",
-  "00/13/2022/0",
-  "00/13/202x",
+const dateArray = [
+  "29-02-2024", // true - 2024 високосный год
+  "29-02-2023", // false - 2023 не високосный год
+  "02/29/2024", // true - EN формат записи 29-02-2024
+  "02/29/2023", // false - EN формат записи 29-02-2023
+  "31-09-2024", // false - 31 сентября не бывает
+  "30-09-2024", // true - Тут проблем нет - корректная дата
+  "30d-09m-2024y", // true / false - корректная дата при условии parseInt для валидации числа, при Number - нет
+  "30.01.2024", // true / false - зависит от парсинга разделителей
 ];
 
 function dateFilter(arr, fn) {
-  const res = arr
-    .map((arrEl) => (arrEl.includes("-") ? arrEl.split("-") : arrEl.split("/")))
-    .filter((arrEl) => fn(arrEl));
-  return res.map((arrEl) => arrEl.join("-"));
+  // const res = arr
+  //   .map((arrEl) => (arrEl.includes("-") ? arrEl.split("-") : arrEl.split("/")))
+  //   .filter((arrEl) => fn(arrEl));
+  const typeEN = arr.map((arrEl) =>
+    arrEl.includes("-") ? arrEl.split("-") : 0
+  );
+  const typeRU = arr.map((arrEl) =>
+    arrEl.includes(".")
+      ? arrEl.split(".")
+      : arrEl.includes("/")
+      ? arrEl.split("/")
+      : 0
+  );
+  return typeRU;
 }
-
-function dateChecker(arr) {
-  const newArr = arr.filter((arrEl) => arrEl > 0);
-  if (newArr.length == 3) {
-    const [day, month, year] = arr;
-    const daysInMonth = [
-      31,
-      year % 4 == 0 || (year % 100 == 0 && year % 400 == 0) ? 29 : 28,
-      31,
-      30,
-      31,
-      30,
-      31,
-      31,
-      30,
-      31,
-      30,
-      31,
-    ];
-    const days = day > 0 && day <= daysInMonth[month - 1];
-    const months = month > 0 && month <= 12;
-    const years = year > 0;
-    return days && months && years;
-  }
-  return false;
-}
-
-console.log(dateFilter(dates, dateChecker));
+console.log(dateFilter(dateArray));
+// function dateChecker(arr) {
+//   const newArr = arr.filter((arrEl) => arrEl > 0);
+//   if (newArr.length == 3) {
+//     const [day, month, year] = arr;
+//     const daysInMonth = [
+//       31,
+//       year % 4 == 0 || (year % 100 == 0 && year % 400 == 0) ? 29 : 28,
+//       31,
+//       30,
+//       31,
+//       30,
+//       31,
+//       31,
+//       30,
+//       31,
+//       30,
+//       31,
+//     ];
+//     const days = day > 0 && day <= daysInMonth[month - 1];
+//     const months = month > 0 && month <= 12;
+//     const years = year > 0;
+//     return days && months && years;
+//   }
+//   return false;
+// }
